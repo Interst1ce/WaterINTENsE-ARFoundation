729955350843f2971f3f2492fbb41e25683325f6
Interst1ce <intersticegamesdev@gmail.com>
2020-09-21 11:17:01 -0500

    Titation Steps
    Added all the steps/targets for Titration including audio and animations
    Made a couple changes to quality settings to get closer to the original version's graphical presentation

---
+++ Assets/Scripts/TitrationClipboardUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TitrationClipboardUI : MonoBehaviour {
+    public TextMeshProUGUI last, first, free;
+    public void FadeUIIn() {
+        StartCoroutine(UIAlphaFade(last,6));
+        StartCoroutine(UIAlphaFade(first,9));
+        StartCoroutine(UIAlphaFade(free,11));
+    }
+
+    IEnumerator UIAlphaFade(TextMeshProUGUI text, float delay = 0) {
+        float t = 0;
+        while (t < delay) {
+            t += Time.deltaTime;
+            yield return null;
+        }
+        t = 0; 
+        Color32 newColor;
+        while (t < 1) {
+            t += Time.deltaTime;
+            newColor = new Color32(0,0,0,(byte)((text.color.a + t) * 255));
+            text.color = newColor;
+            yield return null;
+        }
+        newColor = new Color32(0,0,0,255);
+        text.color = newColor;
+    }
+}
