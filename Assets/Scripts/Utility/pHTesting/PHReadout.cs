51d8864c9d2a5e55dcfc45d515ec8e21c5c5bc6d
Interst1ce <intersticegamesdev@gmail.com>
2020-10-26 10:07:34 -0500

    pHTesting Readout UI

---
+++ Assets/Scripts/Utility/pHTesting/PHReadout.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PHReadout : MonoBehaviour {
+    public TextMeshPro readout;
+
+    bool alt = false;
+
+    public void AlternateTrigger() {
+        if (alt) {
+            pH4Update();
+        } else pH7Update();
+        alt = !alt;
+    }
+
+    public void pH4Update() {
+        readout.text = "4.0";
+    }
+
+    public void pH7Update() {
+        readout.text = "7.0";
+    }
+
+    public void SampleUpdate() {
+        readout.text = "6.2";
+    }
+
+    public void naUpdate() {
+        readout.text = "N/A";
+    }
+}
---
+++ Assets/Scripts/Utility/pHTesting/StirrerReadout.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StirrerReadout : MonoBehaviour {
+    public TextMeshPro readout;
+
+    string text = "";
+    int counter = 0;
+    int targetCount = 0;
+
+    public void Count(int target) {
+        targetCount = target;
+        if (counter < targetCount) {
+            StartCoroutine("CountUp");
+        } else StartCoroutine("CountDown");
+    }
+
+    IEnumerator CountUp() {
+        while (counter <= targetCount) {
+            text = "00" + counter;
+            counter++;
+            readout.text = text;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+    
+    IEnumerator CountDown() {
+        while (counter >= targetCount) {
+            text = "00" + counter;
+            counter--;
+            readout.text = text;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
